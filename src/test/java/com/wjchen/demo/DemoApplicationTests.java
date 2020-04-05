b1ece76384129ef060416c28d4e43cf7993b9d24
Cc221 <wjchen221@163.com>
2020-04-05 21:10:04 +0800

    redis  整合

--- src/test/java/com/wjchen/demo/DemoApplicationTests.java
+++ src/test/java/com/wjchen/demo/DemoApplicationTests.java
@@ -1,10 +1,20 @@
 package com.wjchen.demo;
 
+import com.wjchen.demo.common.utils.RedisUtil;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+    @Autowired
+    private RedisUtil redisUtil;
 
+    @Test
+    public void test(){
+        boolean set = redisUtil.set("a", "this is a");
+        Object a = redisUtil.get("a");
+        System.out.println(a.toString());
+    }
 }
