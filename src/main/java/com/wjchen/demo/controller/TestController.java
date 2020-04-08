1291b9c687bbef7590ea0ee1d3f245e0ae09d39d
cwj <wjchen221@163.com>
2020-04-08 14:49:29 +0800

    测试controller

---
+++ src/main/java/com/wjchen/demo/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.wjchen.demo.controller;
+
+import com.wjchen.demo.base.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+@Controller
+public class TestController extends BaseController {
+
+    @RequestMapping("test")
+    @ResponseBody
+    public String test(){
+        Map<String,Object> paramMap = getParamMap();
+
+        return "wjchen221";
+    }
+
+}
